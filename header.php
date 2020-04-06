243a09c9272b8314315d26ce24c97b65ce2a7254
Andrew Jackson <andrewz.jackson667@gmail.com>
2020-04-06 10:14:10 -0400

    Started working in header.php. Created the header comment and added starter html.

---
+++ header.php
@@ -0,0 +1,43 @@
+<?php 
+    /**
+        * Header file for the Enchanté theme.
+        * @link
+        *
+        * @package WordPress
+        * @subpackage Enchanté
+        * @since 1.0.0
+    **/
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+
+    <title>Enchanté - HOME</title>
+
+    <?php wp_head(); ?>
+
+</head>
+<body <?php body_class(); ?> >
+
+    <?php 
+        if ( function_exists( 'wp_body_open' ) ) {
+            wp_body_open();
+        } else {
+            do_action( 'wp_body_open' );
+        }
+    ?>
+
+    <?php
+        if( function_exists( 'wp_footer();' ) ) {
+            wp_footer();
+        } else {
+            do_action( 'wp_footer')
+        }
+    ?>  
+
+</body>
+</html>
