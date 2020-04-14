ed06f226dab90701cac6db9199bb595ff7a02f1d
Yasuke Oda Nobunaga <Andrew Jackson>
2020-04-14 14:00:52 -0400

    Added to function-admin.php: Registered More Socail Network Settings Fields and functions for Youtube, Instagram, WhatsApp, Pinterest, Reddit, LinkedIn

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -60,7 +60,14 @@
 
         /** REGISTER SETTING - SOCIAL NETWORKS */
         register_setting( 'enchanté_settings_group', 'facebook_handler' );
+        register_setting( 'enchanté_settings_group', 'youtube_handler' );
+        register_setting( 'enchanté_settings_group', 'instagram_handler' );
         register_setting( 'enchanté_settings_group', 'twitter_handler' );
+        register_setting( 'enchanté_settings_group', 'whatsapp_handler' );
+        register_setting( 'enchanté_settings_group', 'pinterest_handler' );
+        register_setting( 'enchanté_settings_group', 'reddit_handler' );
+        register_setting( 'enchanté_settings_group', 'tumblr_handler' );
+        register_setting( 'enchanté_settings_group', 'linkedin_handler' );
         register_setting( 'enchanté_settings_group', 'gplus_handler' );
 
         /**
@@ -77,10 +84,22 @@
 
         /** ADD SETTINGS FIELD - Facebook */
         add_settings_field( 'sidebar-facebook', 'Facebook handler', 'enchanté_sidebar_facebook', 'une_enchanté', 'enchanté_sidebar_options' );
+        /** ADD SETTINGS FIELD - Youtube */
+        add_settings_field( 'sidebar-youtube', 'Youtube handler', 'enchanté_sidebar_youtube', 'une_enchanté', 'enchanté_sidebar_options' );
+        /** ADD SETTINGS FIELD - Instagram */
+        add_settings_field( 'sidebar-instagram', 'Instagram handler', 'enchanté_sidebar_instagram', 'une_enchanté', 'enchanté_sidebar_options' );
         /** ADD SETTINGS FIELD - Twitter */
         add_settings_field( 'sidebar-twitter', 'Twitter handler', 'enchanté_sidebar_twitter', 'une_enchanté', 'enchanté_sidebar_options' );
+        /** ADD SETTINGS FIELD - WhatsApp */
+        add_settings_field( 'sidebar-whatsapp', 'WhatsApp handler', 'enchanté_sidebar_whatsapp', 'une_enchanté', 'enchanté_sidebar_options' );
+        /** ADD SETTINGS FIELD - Pinterest */
+        add_settings_field( 'sidebar-pinterest', 'Pinterest handler', 'enchanté_sidebar_pinterest', 'une_enchanté', 'enchanté_sidebar_options' );
+        /** ADD SETTINGS FIELD - Reddit */
+        add_settings_field( 'sidebar-reddit', 'Reddit handler', 'enchanté_sidebar_reddit', 'une_enchanté', 'enchanté_sidebar_options' );
         /** ADD SETTINGS FIELD - Tumblr */
         add_settings_field( 'sidebar-tumblr', 'Tumblr handler', 'enchanté_sidebar_tumblr', 'une_enchanté', 'enchanté_sidebar_options' );
+        /** ADD SETTINGS FIELD - LinkedIn */
+        add_settings_field( 'sidebar-linkedin', 'LinkedIn handler', 'enchanté_sidebar_linkedin', 'une_enchanté', 'enchanté_sidebar_options' );
         /** ADD SETTINGS FIELD - Google Plus */
         add_settings_field( 'sidebar-gplus', 'Google+ handler', 'enchanté_sidebar_gplus', 'une_enchanté', 'enchanté_sidebar_options' );
     }
@@ -107,13 +126,37 @@
         $facebook = esc_attr( get_option( 'facebook_handler' ) );
         echo '<input type="text" name="facebook_handler" value="'.$facebook.'" placeholder="Facebook handler" />';
     }
+    function enchanté_sidebar_youtube() {
+        $youtube = esc_attr( get_option( 'youtube_handler' ) );
+        echo '<input type="text" name="youtube_handler" value="'.$youtube.'" placeholder="Youtube handler" />';
+    }
+    function enchanté_sidebar_instagram() {
+        $instagram = esc_attr( get_option( 'instagram_handler' ) );
+        echo '<input type="text" name="instagram_handler" value="'.$instagram.'" placeholder="Instagram handler" />';
+    }
     function enchanté_sidebar_twitter() {
         $twitter = esc_attr( get_option( 'twitter_handler' ) );
         echo '<input type="text" name="twitter_handler" value="'.$twitter.'" placeholder="Twitter handler" />';
     }
+    function enchanté_sidebar_whatsapp() {
+        $whatsapp = esc_attr( get_option( 'whatsapp_handler' ) );
+        echo '<input type="text" name="whatsapp_handler" value="'.$whatsapp.'" placeholder="WhatsApp handler" />';
+    }
+    function enchanté_sidebar_pinterest() {
+        $pinterest = esc_attr( get_option( 'pinterest_handler' ) );
+        echo '<input type="text" name="pinterest_handler" value="'.$pinterest.'" placeholder="Pinterest handler" />';
+    }
+    function enchanté_sidebar_reddit() {
+        $reddit = esc_attr( get_option( 'reddit_handler' ) );
+        echo '<input type="text" name="reddit_handler" value="'.$reddit.'" placeholder="Reddit handler" />';
+    }
     function enchanté_sidebar_tumblr() {
         $tumblr = esc_attr( get_option( 'tumblr_handler' ) );
         echo '<input type="text" name="tumblr_handler" value="'.$tumblr.'" placeholder="Tumblr handler" />';
+    }
+    function enchanté_sidebar_linkedin() {
+        $linkedin = esc_attr( get_option( 'linkedin_handler' ) );
+        echo '<input type="text" name="linkedin_handler" value="'.$linkedin.'" placeholder="LinkedIn handler" />';
     }
     function enchanté_sidebar_gplus() {
         $gplus = esc_attr( get_option( 'gplus_handler' ) );
