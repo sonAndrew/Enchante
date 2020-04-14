2a98c227bed63b1e8e27e03c838f64ac2ea89f88
Yasuke Oda Nobunaga <Andrew Jackson>
2020-04-14 13:45:12 -0400

    Added to function-admin.php: Clear comments to make the file more readable. Socail Network Settings Fields for Facebook, Twitter, Tumblr, Google+

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -1,12 +1,19 @@
 <?php
-
-    /***
-    @package enchanté
-        =======================
-            ADMIN PAGE
-        =======================
+    /**
+        @package enchanté
+        * * * * * * * * * * * * * * * * *
+        * * * * * * * * * * * * * * * * *
+        *      ADMIN PAGE
+        * * * * * * * * * * * * * * * * *
+        * * * * * * * * * * * * * * * * *
     */
-
+    /**
+     * * * * * * * * * * * * * * * * *
+     * * * * * * * * * * * * * * * * *
+     *          ADDED PAGES
+     * * * * * * * * * * * * * * * * *
+     * * * * * * * * * * * * * * * * *
+    */
     function enchanté_add_admin_page() {
         /** Generate Enchanté Admin Page */
         add_menu_page( 'Enchanté Theme Options', 'Enchanté', 'manage_options', 'une_enchanté', 'enchanté_theme_create_page', '', 110 );
@@ -21,24 +28,9 @@
         add_action( 'admin_init', 'enchanté_custom_settings' );
     }
     add_action( 'admin_menu', 'enchanté_add_admin_page' );
-
-    function enchanté_custom_settings(){ 
-        register_setting( 'enchanté_settings_group', 'first_name' );
-        register_setting( 'enchanté_settings_group', 'last_name' );
-        add_settings_section( 'enchanté_sidebar_options', 'Sidebar Options', 'enchanté_sidebar_options', 'une_enchanté' );
-        add_settings_field( 'sidebar-name', 'First Name', 'enchanté_sidebar_name', 'une_enchanté', 'enchanté_sidebar_options' );
-    }
-
-    function enchanté_sidebar_options() {
-        /** Generation of our Sidebar */
-        echo '<h1>Customize Your Sidebar Information</h1>';
-    }
-
-    function enchanté_sidebar_name() {
-        $firstName = esc_attr( get_option( 'first_name' ) );
-        echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name" />';
-    }
-
+    /* * * * * * * * * * * * * * * * * * * * 
+     *           SUB PAGES 
+    * * * * * * * * * * * * * * * * * * * */
     function enchanté_theme_create_page() {
         /** Generation of our admin page */
         require_once( get_template_directory() . '/inc/templates/enchanté-admin.php' );
@@ -47,4 +39,83 @@
         /** Generation of our Custom CSS page */
         echo '<h1>Enchanté Custom CSS</h1>';
     }
-    
+    /**
+     * * * * * * * * * * * * * * * * *
+     * * * * * * * * * * * * * * * * *
+     *   ENCHANTE CUSTOME SETTINGS
+     * * * * * * * * * * * * * * * * *
+     * * * * * * * * * * * * * * * * *
+    */
+    function enchanté_custom_settings(){ 
+        /**
+         * * * * * * * * * * * * * * * * *
+         * * * * * * * * * * * * * * * * *
+         *      SETTINGS REGISTRATION
+         * * * * * * * * * * * * * * * * *
+         * * * * * * * * * * * * * * * * *
+        */
+        /** REGISTER SETTING - FULL NAME */
+        register_setting( 'enchanté_settings_group', 'first_name' );
+        register_setting( 'enchanté_settings_group', 'last_name' );
+
+        /** REGISTER SETTING - SOCIAL NETWORKS */
+        register_setting( 'enchanté_settings_group', 'facebook_handler' );
+        register_setting( 'enchanté_settings_group', 'twitter_handler' );
+        register_setting( 'enchanté_settings_group', 'gplus_handler' );
+
+        /**
+         * * * * * * * * * * * * * * * * *
+         * * * * * * * * * * * * * * * * *
+         *      SETTINGS ADDED
+         * * * * * * * * * * * * * * * * *
+         * * * * * * * * * * * * * * * * *
+        */
+        add_settings_section( 'enchanté_sidebar_options', 'Sidebar Options', 'enchanté_sidebar_options', 'une_enchanté' );
+        /** Settings field for Full Name */
+        add_settings_field( 'sidebar-name', 'Full Name', 'enchanté_sidebar_name', 'une_enchanté', 'enchanté_sidebar_options' );
+
+
+        /** ADD SETTINGS FIELD - Facebook */
+        add_settings_field( 'sidebar-facebook', 'Facebook handler', 'enchanté_sidebar_facebook', 'une_enchanté', 'enchanté_sidebar_options' );
+        /** ADD SETTINGS FIELD - Twitter */
+        add_settings_field( 'sidebar-twitter', 'Twitter handler', 'enchanté_sidebar_twitter', 'une_enchanté', 'enchanté_sidebar_options' );
+        /** ADD SETTINGS FIELD - Tumblr */
+        add_settings_field( 'sidebar-tumblr', 'Tumblr handler', 'enchanté_sidebar_tumblr', 'une_enchanté', 'enchanté_sidebar_options' );
+        /** ADD SETTINGS FIELD - Google Plus */
+        add_settings_field( 'sidebar-gplus', 'Google+ handler', 'enchanté_sidebar_gplus', 'une_enchanté', 'enchanté_sidebar_options' );
+    }
+    /**
+     * * * * * * * * * * * * * * * * *
+     * * * * * * * * * * * * * * * * *
+     *      SETTINGS FUNCTIONS
+     * * * * * * * * * * * * * * * * *
+     * * * * * * * * * * * * * * * * *
+    */ 
+    function enchanté_sidebar_options() {
+        /** Generation of our Sidebar */
+        echo '<h1>Customize Your Sidebar Information</h1>';
+    }
+    function enchanté_sidebar_name() {
+        $firstName = esc_attr( get_option( 'first_name' ) );
+        $lastName = esc_attr( get_option( 'last_name' ) );
+        echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name" /> <input type="text" name="last_name" value="'.$lastName.'" placeholder="Last Name" />';
+    }
+    /* * * * * * * * * * * * * * * * * * * * 
+     *          SOCIAL NETWORKS 
+    * * * * * * * * * * * * * * * * * * * */
+    function enchanté_sidebar_facebook() {
+        $facebook = esc_attr( get_option( 'facebook_handler' ) );
+        echo '<input type="text" name="facebook_handler" value="'.$facebook.'" placeholder="Facebook handler" />';
+    }
+    function enchanté_sidebar_twitter() {
+        $twitter = esc_attr( get_option( 'twitter_handler' ) );
+        echo '<input type="text" name="twitter_handler" value="'.$twitter.'" placeholder="Twitter handler" />';
+    }
+    function enchanté_sidebar_tumblr() {
+        $tumblr = esc_attr( get_option( 'tumblr_handler' ) );
+        echo '<input type="text" name="tumblr_handler" value="'.$tumblr.'" placeholder="Tumblr handler" />';
+    }
+    function enchanté_sidebar_gplus() {
+        $gplus = esc_attr( get_option( 'gplus_handler' ) );
+        echo '<input type="text" name="gplus_handler" value="'.$gplus.'" placeholder="Google+ handler" />';
+    }
