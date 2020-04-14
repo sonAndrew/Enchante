2c408ae3054b8e53185922459f19e8a15a7ddffd
Andrew Jackson <andrewz.jackson667@gmail.com>
2020-04-14 10:38:33 -0400

    Changed screenshoot.php to bigger screenshot. | Added form to enchante-admin.php

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -16,8 +16,15 @@
         add_submenu_page( 'une_enchanté', 'Enchanté Theme Options', 'General', 'manage_options', 'une_enchanté', 'enchanté_theme_create_page' ); /** General */
 
         add_submenu_page( 'une_enchanté', 'Enchanté CSS Options', 'Custom CSS', 'manage_options', 'une_enchanté_css', 'enchanté_theme_settings_page' ); /** Custom CSS */
+
+        /** Activate Custom Settings */
+        add_action( 'admin_init', 'enchanté_custom_settings' );
     }
     add_action( 'admin_menu', 'enchanté_add_admin_page' );
+    
+    function enchanté_custom_settings(){ 
+        register_setting( 'enchanté_settings_group', 'first_name' );
+    }
 
     function enchanté_theme_create_page() {
         /** Generation of our admin page */
--- inc/templates/enchanté-admin.php
+++ inc/templates/enchanté-admin.php
@@ -1,4 +1,8 @@
 <h1>Echanté Theme Options</h1>
 <h3 class="title">Manage Options</h3>
-<p>Customize the default WordPress Appearance Options</p>
-<?php bloginfo( 'name' ); ?>
+<p>Customize sidebar Options</p>
+
+<form method="post" action="">
+    <?php settings_fields( 'enchanté_settings_group' ) ?>
+    add_settings_section( 'enchanté_sidebar_options', 'Sidebar Options', 'enchanté_sidebar_options', 'une_enchanté' );
+</form>
