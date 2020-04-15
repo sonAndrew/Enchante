ea9beea674005bf8be7d2ed74243b687b130d449
Yasuke Oda Nobunaga <Andrew Jackson>
2020-04-14 20:32:57 -0400

    Added to enchanté-admin.php: A container div for the admin section. | Added to enchanté-admin.css: Sample stying to the layout.

--- inc/templates/enchanté-admin.php
+++ inc/templates/enchanté-admin.php
@@ -16,26 +16,28 @@
     $whatsapp = esc_attr( get_option( 'whatsapp_handler' ) );
 ?>
 
-<div class="enchanté-sidebar-preview">
-    <div class="enchanté-sidebar">
-        <h3 class="enchanté-username"><?php print $fullName; ?></h3>
-        <p class="enchanté-user-description"><?php print $description; ?></p>
-        <p class="enchanté-job-title"><?php print $job; ?></p>
-        <p class="enchanté-user-location"><?php print $location; ?></p>
-        <div class="enchanté-user-social-wrapper">
-            <ul>
-                <li><a href=""><?php print $facebook; ?></a></li>
-                <li><a href=""><?php print $youtube; ?></a></li>
-                <li><a href=""><?php print $instagram; ?></a></li>
-                <li><a href=""><?php print $twitter; ?></a></li>
-                <li><a href=""><?php print $whatsapp; ?></a></li>
-            </ul>
+<div class="enchanté-admin-container">
+    <div class="enchanté-sidebar-preview">
+        <div class="enchanté-sidebar">
+            <h1 class="enchanté-username"><?php print $fullName; ?></h1>
+            <h4 class="enchanté-user-description"><?php print $description; ?></h4>
+            <h4 class="enchanté-job-title"><?php print $job; ?></h4>
+            <h4 class="enchanté-user-location"><?php print $location; ?></h4>
+            <div class="enchanté-user-social-container">
+                <ul>
+                    <li><a href=""><?php print $facebook; ?></a></li>
+                    <li><a href=""><?php print $youtube; ?></a></li>
+                    <li><a href=""><?php print $instagram; ?></a></li>
+                    <li><a href=""><?php print $twitter; ?></a></li>
+                    <li><a href=""><?php print $whatsapp; ?></a></li>
+                </ul>
+            </div>
         </div>
     </div>
-</div>
 
-<form method="post" action="options.php" class="enchanté-general-form">
-    <?php settings_fields( 'enchanté_settings_group' ); ?>
-    <?php do_settings_sections( 'une_enchanté' ); ?>
-    <?php submit_button(); ?>
-</form>
+    <form method="post" action="options.php" class="enchanté-general-form">
+        <?php settings_fields( 'enchanté_settings_group' ); ?>
+        <?php do_settings_sections( 'une_enchanté' ); ?>
+        <?php submit_button(); ?>
+    </form>
+</div>
